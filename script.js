b1f31c3ac29f8cf340e3adf093a6b12784c2ec8e
aarpatil09-lgtm <aarpatil09@gmail.com>
2026-04-28 15:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	let count = text.toLowerCase();
+	let count1 = text.toUpperCae();
+	return count + "-" + count1;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
